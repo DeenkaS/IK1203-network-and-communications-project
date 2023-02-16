53dc79b0db5d0fc33590af0caf666132499b6204
Deniel Saskin <Deniel.saskin@gmail.com>
2023-02-16 12:21:02 +0100

    havent tested yet. still fixing.

--- task2/tcpclient/TCPClient.java
+++ task2/tcpclient/TCPClient.java
@@ -16,20 +16,21 @@ public class TCPClient {
 
     public byte[] askServer(String hostname, int port, byte[] toServerBytes) throws IOException {
         Socket client = new Socket(hostname, port);
-        client.setSoTimeout(timeout);
+        if(this.timeout != null)
+        client.setSoTimeout(this.timeout);
         ByteArrayOutputStream output = new ByteArrayOutputStream();
 
         try {
             output.toByteArray();
 
             client.getOutputStream().write(toServerBytes);
-            if (shutdown) {
+            if (this.shutdown) {
                 client.shutdownOutput();
             }
 
             if (limit != null) {
 
-                output.write(client.getInputStream().readNBytes(limit));
+                output.write(client.getInputStream().readNBytes(this.limit));
             } else {
                 output.write(client.getInputStream().readAllBytes());
             }
