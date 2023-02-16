d4485703ec61c3be9af2abb605d826989462904a
Deniel Saskin <Deniel.saskin@gmail.com>
2023-02-16 12:14:47 +0100

    first draft of task 2. will test in a bit. if no other commit im done.

--- task2/tcpclient/TCPClient.java
+++ task2/tcpclient/TCPClient.java
@@ -1,13 +1,44 @@
 package tcpclient;
+
 import java.net.*;
 import java.io.*;
 
 public class TCPClient {
-    
+    Integer timeout;
+    Integer limit;
+    boolean shutdown;
+
     public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        this.shutdown = shutdown;
+        this.timeout = timeout;
+        this.limit = limit;
     }
 
-    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
-        return new byte[0];
+    public byte[] askServer(String hostname, int port, byte[] toServerBytes) throws IOException {
+        Socket client = new Socket(hostname, port);
+        client.setSoTimeout(timeout);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        try {
+            output.toByteArray();
+
+            client.getOutputStream().write(toServerBytes);
+            if (shutdown) {
+                client.shutdownOutput();
+            }
+
+            if (limit != null) {
+
+                output.write(client.getInputStream().readNBytes(limit));
+            } else {
+                output.write(client.getInputStream().readAllBytes());
+            }
+
+        } catch (SocketTimeoutException e) {
+            client.close();
+            return output.toByteArray();
+        }
+        client.close();
+        return output.toByteArray();
     }
 }
